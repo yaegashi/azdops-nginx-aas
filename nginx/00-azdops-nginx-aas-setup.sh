31e42c5e3981b7141067d3084a911c1572a914f5
YAEGASHI Takeshi <yaegashi@gmail.com>
2025-03-23 23:23:48 +0000

    Remove nginx-build.yml workflow

--- nginx/00-azdops-nginx-aas-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p /nginx/data /nginx/templates
-rm -rf /data /etc/nginx/templates
-ln -sf /nginx/data /data
-ln -sf /nginx/templates /etc/nginx/templates
-watch-nginx-config.sh /etc/nginx/templates &
-exec /usr/sbin/sshd
